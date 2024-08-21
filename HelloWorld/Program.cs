00bec7060e7416d7a4af20c9ca48d7bcab4937f2
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-08-21 11:00:49 -0700

    All Done

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your name?");
+var teacherName = Console.ReadLine();
+Console.WriteLine($"Hello, {teacherName}! How are you today?");
